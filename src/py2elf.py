9ff4ef474958e0bb7280edae8b46e917d048c295
toto112358 <luca@DESKTOP-VDH2DD6.localdomain>
2020-12-04 20:16:13 +0100

    Edited default cython command

--- src/py2elf.py
+++ src/py2elf.py
@@ -4,13 +4,14 @@ import argparse
 import os
 import random
 import string
-if sys.version_info.major != 3:
+if sys.version_info.major != 3:                                     # Check if we're using python 3.X
     exit('[ERROR]: You must be using Python 3.X to run this script')
 pypath=f'/usr/include/python3.{sys.version_info.minor}'
 def get_random_string(length):
     letters = string.ascii_lowercase
     result_str = ''.join(random.choice(letters) for i in range(length))
     return result_str 
+arch=os.popen('uname -p').read()                                    # We want to know cpu architecture for compiling
 random_temp=get_random_string(20)                                   # We use this variable to make sure some the temporary file we create don't already exist
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', help='out file')
@@ -18,6 +19,6 @@ parser.add_argument('file_to_compile', help='Select python3 file to compile to E
 args = parser.parse_args()
 in_file=args.file_to_compile
 output=args.o
-os.system(f'cython3 -o /tmp/{random_temp}.c --embed {in_file}')
+os.system(f'cython3 -3 -o /tmp/{random_temp}.c --embed {in_file}')
 os.system(f'gcc -Os -I {pypath} /tmp/{random_temp}.c -o {output} -lpython3.8 -lpthread -lm -lutil -ldl')
 os.system(f'rm /tmp/{random_temp}.c')
